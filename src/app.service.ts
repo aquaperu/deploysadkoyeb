26d0da9bfb244ee9830810a32efb92bf1598f80c
ingdantev <ingdantev@gmail.com>
2024-10-02 13:55:34 +0000

    add .env

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,11 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 export const configLoader = ()=>{
   return {
-      port: process.env.PORT,
-      database:{
-          username:process.env.DATABASE_USERNAME,
-          password:process.env.DATABASE_PASSWORD
-      },
+      
       mongo:{
           uri:process.env.MONGO_URI
       },
