568e4f9852fb35e740fc15753e8ec903b76cdcce
ingdantev <ingdantev@gmail.com>
2024-10-02 13:45:56 +0000

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { configLoader } from 'config-loader';
 
+export const configLoader = ()=>{
+  return {
+      port: process.env.PORT,
+      database:{
+          username:process.env.DATABASE_USERNAME,
+          password:process.env.DATABASE_PASSWORD
+      },
+      mongo:{
+          uri:process.env.MONGO_URI
+      },
+      jwt:process.env.JWT_SECRET
+  }
+}
 @Injectable()
 export class AppService {
   getHello(): string {
