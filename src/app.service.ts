88d1a41d006736864b9feeb4181620ae206a6024
ingdantev <ingdantev@gmail.com>
2024-10-02 13:31:39 +0000

    agrega el fichero configuracion

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { configLoader } from 'config-loader';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! inicial';
+    const lo=configLoader
+    return `la configuracion ${lo}`;
   }
 }
